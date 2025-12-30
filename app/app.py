15f0d98bffd39d74be9ce70e28488e02ebeae619
joaobmt <joaotouraes18@gmail.com>
2025-12-30 23:31:41 +0000

    feat: mudanca para v2 para mostrar rolling update

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ def hello():
     user = os.getenv('APP_USER', 'Visitante')
     secret_code = os.getenv('APP_SECRET', 'Sem Segredo')
     
-    return f"Olá {user}! O segredo é: {secret_code} - Versão: v1"
+    return f"Olá {user}! O segredo é: {secret_code} - Versão: v2"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
