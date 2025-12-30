348d850cfd54556da2a35f1d291461f13e0db5ea
joaobmt <joaotouraes18@gmail.com>
2025-12-30 23:27:38 +0000

    refactor: mudanças no codigo para funcionar melhor

--- app/app.py
+++ app/app.py
@@ -1,11 +1,14 @@
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return f"Hello DevOps! ENV={os.getenv('APP_ENV')}"
+@app.route('/')
+def hello():
+    user = os.getenv('APP_USER', 'Visitante')
+    secret_code = os.getenv('APP_SECRET', 'Sem Segredo')
+    
+    return f"Olá {user}! O segredo é: {secret_code} - Versão: v1"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
